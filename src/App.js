ce385d8f576829bd957dc4154edc97ada290d98d
Ann Catherine Jose <ann.jose@gmail.com>
2021-03-06 07:50:19 -0800

    Render sub component header as Task Tracker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
 
-  const name = "Ann"
   return (
     <div className="App">
-      <h1> Hello React to {name} </h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+
+    return (
+        // the following i HTML header tag should be lowercase otherwise React treats it as JSX
+        <header>
+            <h1>Task Tracker</h1>
+        </header>
+    )
+}
+
+export default Header
